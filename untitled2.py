38a0eeebd9d6ce7565f058b1dad8fee25fb73190
sdkdk10 <1995jihee@hanmail.net>
2016-06-15 20:20:33 +0900

    eighth

--- untitled2.py
+++ untitled2.py
@@ -4,31 +4,7 @@ import urllib.request
 import xml.etree.ElementTree as etree
 
 loopFlag = 1
-
-#-------------------------------도시코드-----------------------------------------------
-def cityCode():
-
-    key = 'GH9cfIKgPs69CGQioE5A2dYp9V1P8OCywu%2BnaanIOWiTue3FlroqDCEuWo4k8ekz%2F91Wlhpx%2Bwl6kfHWTG0EAg%3D%3D'
-    url = "http://openapi.tago.go.kr/openapi/service/ArvlInfoInqireService/getCtyCodeList?ServiceKey=" + key
-
-    data = urllib.request.urlopen(url).read()
-
-    filename = "citycode.xml"
-    f = open(filename, "wb")
-    f.write(data)
-    f.close()
-    
-    #파싱하기
-    tree = etree.parse(filename)    
-    itemElements = tree.getiterator("item")
-    print("items")
-    for item in itemElements:
-        #if(item.find("infoType")).text == "1":
-        code = item.find("code")
-        sname = item.find("sname")
-
-        print(str(code.text) + "\t" + str(sname.text))
-
+subwayFlag = 1
 
 #--------------------------------지하철 전채 첫/막차 시간--------------------------------------------------------------
 def subway(End, Line, Day, Inout):
@@ -47,25 +23,25 @@ def subway(End, Line, Day, Inout):
     itemElements = tree.getiterator("row")
     print("외부코드\t전철역코드\t전철역\t첫차시간\t첫차출발역\t막차시간\t막차출발역\t막차도착역")
     for item in itemElements:
-        #if(item.find("infoType")).text == "1":
-#        LINE_NUM = item.find("LINE_NUM")                 #호선
-#        WEEK_TAG = item.find("WEEK_TAG")                 #요일
-#        INOUT_TAG = item.find("INOUT_TAG")               #상/하행선
         FR_CODE = item.find("FR_CODE")                   #외부코드
         STATION_CD = item.find("STATION_CD")             #전철역코드
         STATION_NM = item.find("STATION_NM")             #전철역명
         FIRST_TIME = item.find("FIRST_TIME")             #첫차시간
-#        F_ORIGINSTATION = item.find("F_ORIGINSTATION")   #첫차출발역코드
         F_SUBWAYSNAME = item.find("F_SUBWAYSNAME")       #첫차출발역명
         LAST_TIME = item.find("LAST_TIME")               #막차시간
-#        L_ORIGINSTATION = item.find("L_ORIGINSTATION")   #막차출발역코드
         L_SUBWAYSNAME = item.find("L_SUBWAYSNAME")       #막차출발역명
-#        L_DESTSTATION = item.find("L_DESTSTATION")       #막차도착역코드
         L_SUBWAYENAME = item.find("L_SUBWAYENAME")       #막차도착역명
             
         print(str(FR_CODE.text) + "\t" + str(STATION_CD.text)+  "\t" + str(STATION_NM.text)
         + "\t" + str(FIRST_TIME.text) + "\t" + str(F_SUBWAYSNAME.text) + "\t" + str(LAST_TIME.text)  
         + "\t" + str(L_SUBWAYSNAME.text) + "\t" + str(L_SUBWAYENAME.text))
+        
+    mail = input("메일? y/n ")
+    if mail == 'y':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept, filename)
+    elif mail == 'n':
+            print("ok....")
     
 #---------------------------------지하철역막차검색----------------------------------------------------------------
 def subwaySearch(Code, Day, Inout):
@@ -92,6 +68,13 @@ def subwaySearch(Code, Day, Inout):
         LEFTTIME = item.find("LEFTTIME")                 #출발시간
             
         print(str(FR_CODE.text) + "\t" + str(STATION_CD.text)+  "\t" + str(STATION_NM.text) + "\t" + str(SUBWAYENAME.text) + "\t" + str(LEFTTIME.text))
+        
+    mail = input("메일? y/n ")
+    if mail == 'y':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept, filename)
+    elif mail == 'n':
+            print("ok....")
 #-------------------------------------------------------------------------------------------
 #-------------------------------------한글 utf-8 변환후 바꾸기----------------------------------
 def Change(Name):
@@ -102,7 +85,6 @@ def Change(Name):
     subwayMakeaSearch(Name)
 #-----------------------------------역 검색해서 찾기----------------------------------------
 def subwayMakeaSearch(StationName):
-    print(StationName)
     key = "GH9cfIKgPs69CGQioE5A2dYp9V1P8OCywu%2BnaanIOWiTue3FlroqDCEuWo4k8ekz%2F91Wlhpx%2Bwl6kfHWTG0EAg%3D%3D"
     url = "http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getKwrdFndSubwaySttnList?ServiceKey=" + key + "&subwayStationName=" + StationName
     
@@ -123,16 +105,24 @@ def subwayMakeaSearch(StationName):
         subwaystationname = item.find("subwaystationname")             #역이름
             
         print(str(subwaystationid.text) + "\t" + str(subwaystationname.text) )
-        
-    print("막차검색:  l")
-    print("전체시간표검색: f")
-    print("출구별 역주변 시설보기: a")
-    subwayKey = str(input ('select menu :'))
-    subwayMenuFun(subwayKey)
+    global subwayFlag
+    while (subwayFlag > 0):    
+        print("\n")
+        print("===Select Menu===")
+        print("막차검색:  l")
+        print("전체시간표검색: f")
+        print("출구별 역주변 시설보기: a")
+        print("출구별 버스: b")
+        print("나가기: q")
+        print("=================")
+        subwayKey = str(input ('select menu :'))
+        subwayMenuFun(subwayKey)
+    
+    subwayFlag = 1
 #---------------------------------역 검색 후 메뉴기능-------------------------------------
 def subwayMenuFun(menu):
     if menu == 'l':
-        Code = input("검색할 역코드를 입력하세요: ")
+        Code = input("검색할 역코드를 입력하세요(SES제외): ")
         Day = input("요일(평:1, 토: 2, 일/공: 3): ")
         Inout = input("상(1)/하(2))행선: ")
         subwaySearch(Code, Day, Inout)
@@ -144,6 +134,14 @@ def subwayMenuFun(menu):
     elif menu == 'a':
         Code = input("검색할 역코드를 입력하세요: ")
         subwayAround(Code)
+    elif menu == 'b':
+        Code = input("검색할 역코드를 입력하세요: ")
+        subwayBus(Code)
+    elif menu == 'q':
+        global subwayFlag
+        subwayFlag = 0
+    else:
+        print("error!")
     
 #------------------------------------역 검색 후 전체시간표--------------------------------
 def subwayFull(Code, Day, InOut):
@@ -166,6 +164,13 @@ def subwayFull(Code, Day, InOut):
         endsubwaystationnm = item.find("endsubwaystationnm")             #종점역이름
             
         print(str(arrtime.text) + "\t" + str(endsubwaystationnm.text))
+    
+    mail = input("메일? y/n ")
+    if mail == 'y':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept, filename)
+    elif mail == 'n':
+            print("ok....")
 
         
 #---------------------------------------역 출구별 건물--------------------------------------
@@ -189,7 +194,42 @@ def subwayAround(Code):
         dirdesc = item.find("dirdesc")             #주변건물
             
         print(str(exitno.text) + "\t" + str(dirdesc.text))
+        
+    mail = input("메일? y/n ")
+    if mail == 'y':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept, filename)
+    elif mail == 'n':
+            print("ok....")
 
+#----------------------------------------역 출구별 버스-------------------------------------------
+def subwayBus(Code):
+    key = "GH9cfIKgPs69CGQioE5A2dYp9V1P8OCywu%2BnaanIOWiTue3FlroqDCEuWo4k8ekz%2F91Wlhpx%2Bwl6kfHWTG0EAg%3D%3D"
+    url = "http://openapi.tago.go.kr/openapi/service/SubwayInfoService/getSubwaySttnExitAcctoBusRouteList?ServiceKey=" + key + "&subwayStationId=" + Code + "&numOfRows=999&pageSize=999&pageNo=1&startPage=1"
+    
+    data = urllib.request.urlopen(url).read()
+
+    filename = "subwayBus.xml"
+    f = open(filename, "wb") 
+    f.write(data)
+    f.close()
+    
+    #파싱하기
+    tree = etree.parse(filename)    
+    itemElements = tree.getiterator("item")
+    print("출구번호\t버스")
+    for item in itemElements:
+        exitno = item.find("exitno")                     #출구번호
+        busrouteno = item.find("busrouteno")             #버스번호
+            
+        print(str(exitno.text) + "\t" + str(busrouteno.text))
+        
+    mail = input("메일? y/n ")
+    if mail == 'y':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept, filename)
+    elif mail == 'n':
+        print("ok....")
 #---------------------------------------버스 검색---------------------------------------    
 def BusSearch(BuscityCode, NodeId):
 #http://openapi.tago.go.kr/openapi/service/서비스명(영문)/오퍼레이션명(영문)
@@ -221,27 +261,66 @@ def BusSearch(BuscityCode, NodeId):
         
         print(str(arrprevstationcnt.text) + "\t" + str(arrtime.text) + "\t" + str(nodeid.text) + "\t" + str(nodenm.text) + "\t" + str(routeid.text) + "\t" + str(routeno.text) + "\t" +  str(routetp.text) )
 
+        
+        mail = input("메일? y/n ")
+        if mail == 'y':
+            reciept = input("받는 사람 메일 주소: ")
+            SendingMail(reciept, filename)
+        elif mail == 'n':
+            print("ok....")
 #-------------------------------------------------------------------------------------------
 
+def SendingMail(reciept, filename):
+    import mimetypes
+    import mysmtplib
+    from email.mime.base import MIMEBase
+    from email.mime.text import MIMEText
+    
+    #global value
+    host = "smtp.gmail.com" # Gmail STMP 서버 주소.
+    port = "587"
+    htmlFileName = filename
+    
+    senderAddr = "1995jihee@gmail.com"     # 보내는 사람 email 주소.
+    recipientAddr = str(reciept)   # 받는 사람 email 주소.
+    
+    msg = MIMEBase("multipart", "alternative")
+    msg['Subject'] = "Term Project"
+    msg['From'] = senderAddr
+    msg['To'] = recipientAddr
+    
+    # MIME 문서를 생성합니다.
+    htmlFD = open(htmlFileName, 'rb')
+    HtmlPart = MIMEText(htmlFD.read(), _charset = 'UTF-8' )
+    htmlFD.close()
+    
+    # 만들었던 mime을 MIMEBase에 첨부 시킨다.
+    msg.attach(HtmlPart)
+    
+    # 메일을 발송한다.
+    s = mysmtplib.MySMTP(host,port)
+    s.set_debuglevel(1)        # 디버깅이 필요할 경우 주석을 푼다.
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login("1995jihee@gmail.com","ghdlghdl11~")
+    s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+    s.close()
+    
 #--------------------------------------매뉴----------------------------------------------
 def printMenu():
     print("\t")
-    print("막차시간정보")
     print("========Menu==========")    
-    print("도시코드:  l")
     print("역 키워드검색: k")
     print("지하철 호선별 막차: s")
-    print("지하철역막차검색: g")
     print("버스: b")
+    print("끝: q")
     print("========Menu==========")
     
 #---------------------------------메뉴실행---------------------------------------------------------
 def launcherFunction(menu):
-    #도시코드
-    if menu ==  'l':
-        cityCode()
     #역 키워드검색
-    elif menu == 'k':
+    if menu == 'k':
         StationName = input("키워드 입력: ")
         StationName = StationName.encode('utf-8')
         print(StationName)
@@ -294,6 +373,13 @@ def launcherFunction(menu):
         BuscityCode = input("도시코드: ")
         NodeId = input("정류소: ")
         BusSearch(BuscityCode, NodeId)
+    elif menu == 'm':
+        reciept = input("받는 사람 메일 주소: ")
+        SendingMail(reciept)
+    elif menu == 'q':
+        print("Thank you! bye~~!")
+        global loopFlag
+        loopFlag = 0
     else:
         print ("error : unknow menu key")
 
